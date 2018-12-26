a4b9600d5831061c6b176e7fd4a218aa8e4900f0
chaddhaji <aakashchaddha@gmail.com>
2018-12-27 00:24:58 +0530

    Including Interface

---
+++ WeatherApp/src/app/weather/weather.service.ts
@@ -0,0 +1,18 @@
+interface ICurrentWeatherData {
+    weather: [{
+        description: string,
+        icon :  string
+    }],
+
+    main: {
+        temp: number
+    },
+
+    sys: {
+        country: string
+    },
+    dt : number,
+    name : string,
+
+
+}
